213eadd18cb52f11696a7143f8bff3b1b532bbe6
Bruno Vidal <brunokasprik@gmail.com>
2023-01-04 20:32:14 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Interface from "./components/Interface.component";
+
+import "./App.css";
+
+
+
+function App() {
+  return (
+    <>
+      <Interface /> 
+      
+    </>
+  );
+}
+
+export default App;
